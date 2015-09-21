5d96d2f13c2837e5ad716312cf362b7c2a70c1c3
Dr. Mark E. Lehr <ml1150258@CSC11.(none)>
2015-09-20 17:50:07 -0700

    CNV

--- Hmwk/cnvftin.s
+++ Hmwk/cnvftin.s
@@ -6,9 +6,9 @@
 .global _start
 _start:
      MOV R1, #108        @BP  0 WD 7
-     MOV R2, #0x00155556 @BP-24 WD24
+     LDR R2, =0x155556   @BP-24 WD24
      MUL R0, R2,R1       @BP-24 WD31
-     LSL R0, #24         @BP  0 WD 7
+     LSR R0, #24         @BP  0 WD 7
      MOV R7, #1
      SWI 0
 
